a2cf6fbe7240f9c1ffa071cb72226a3a277c178a
Shilpa <shilpa.kulkarni@spirent.com>
2014-07-20 20:24:35 -0700

    adding files file

---
+++ test.rb
@@ -0,0 +1,20 @@
+#!C:\Ruby193\bin\ruby.exe
+require 'find'
+puts "Content-type: text/html"
+puts ""
+puts "<html>"
+puts "<body>"
+puts "Test Ruby Page."
+@test = Dir[File.expand_path("C:/Git-workspace/DemoRepo/bookmarks/**/*", __FILE__)].sort_by{|f| File.mtime(f) }.reverse!
+puts @test
+#Find.find ('../.') do |f|
+ #    type = case 
+  #   when File.file?(f) then "F"
+   #  when File.directory?(f) then "D"
+    # else "?"
+     #end
+#puts "#{type} is #{f}"
+#puts "\n"
+#end
+puts "</body>"
+puts "</html>"
